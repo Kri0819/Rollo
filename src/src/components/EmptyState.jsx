79146e9a9114e648c3cc578a0fdc9a3a59b0d0c8
CyM <tina2013miao@gmail.com>
2026-07-11 20:13:16 +0800

    Delete src directory

--- src/src/components/EmptyState.jsx
+++
@@ -1,9 +0,0 @@
-export default function EmptyState({ title, subtitle }) {
-  return (
-    <div className="empty">
-      <div className="empty-ball" />
-      <h2>{title}</h2>
-      <p>{subtitle}</p>
-    </div>
-  );
-}
